f80e037f08158173a4b638da198902cdfb6f9e50
Dave Kozma <dkozma@adobe.com>
2021-02-28 19:56:11 -0500

    Made hero section responsive

--- themes/hugo-theme-c2pa/assets/css/tailwind.config.js
+++ themes/hugo-theme-c2pa/assets/css/tailwind.config.js
@@ -4,6 +4,16 @@ module.exports = {
       colors: {
         base: "#231F20",
       },
+      scale: {
+        55: ".55",
+        60: ".6",
+        65: ".65",
+        70: ".7",
+        80: ".8",
+        85: ".85",
+        90: ".9",
+        95: ".95",
+      },
     },
     fontFamily: {
       sans: ["halcom", "sans-serif"],
